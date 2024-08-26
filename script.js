0aac2e5c50ce024e6545ab449b1f0385257bb3b4
Ashneet Kaur <83127542+ashneeeet@users.noreply.github.com>
2024-08-26 16:23:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function restoreImage(element) {
         element.setAttribute('src', originalSrc);
     }
 }
+
+
+document.addEventListener('mousemove', function(e) {
+    createSparkle(e.pageX, e.pageY);
+});
+
+function createSparkle(x, y) {
+    const sparkle = document.createElement('div');
+    sparkle.className = 'sparkle';
+    document.querySelector('.sparkle-container').appendChild(sparkle);
+
+    sparkle.style.left = x + 'px';
+    sparkle.style.top = y + 'px';
+
+    setTimeout(() => {
+        sparkle.remove();
+    }, 500);
+}
